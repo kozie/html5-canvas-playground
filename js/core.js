d9ba2f6eb866cafd559dca021e69835b58575bdd
Kozie <flamefingers@gmail.com>
2011-08-19 16:04:09 +0000

    Added some code, canvas has now working example

--- js/core.js
+++ js/core.js
@@ -3,7 +3,70 @@
  * @author Koos van Egmond <flamefingers at gmail dot com>
  */
  
-var core = function() {
+(function(win, doc, undef) {
     
+    // Define canvax
+    function canvax() {
+        return new canvax.fn.init();
+    }
     
-};
+    // Set iniitial canvax function
+    canvax.fn = canvax.protoype = {
+      
+        constructor: canvax,
+        
+        canvas: false,
+        context: false,
+        
+        init: function() {
+            
+            // Set the context
+            this.canvas  = doc.getElementById('canvax');
+            this.context = this.canvas.getContext('2d');
+          
+            return this;
+        }
+    };
+    
+    // Link prototypes
+    canvax.fn.init.prototype = canvax.fn;
+    
+    // Draw square method
+    canvax.fn.drawSquare = function(x, y, w, h) {
+        
+        this.context.fillRect(x, y, w, h);
+        return this;
+    };
+    
+    // Draw line method
+    canvax.fn.drawLine = function(x, y, locs, color, width) {
+      
+        // Set start
+        this.context.moveTo(x, y);
+        
+        // Set default color and width
+        if (color === undef) color = '#000';
+        if (width === undef) width = 1;
+        
+        // Loop through the positions and draw
+        var idx, entry;
+        for (idx in locs) {
+            
+            entry = locs[idx];
+            x = entry[0];
+            y = entry[1];
+            
+            this.context.lineTo(x, y);
+        }
+        
+        this.context.strokeStyle = color;
+        this.context.lineWidth = width;
+        this.context.stroke();
+        
+        return this;
+    };
+    
+    // Link canvax function to global scope
+    win.canvax = canvax;
+    
+})(window, window.document);
